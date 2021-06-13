37bf33ed2cabc9d5e861b53a24395ae986f87419
Novikov Kirill <kirya.novikov.03@list.ru>
2021-06-13 17:52:54 +0600

    add exception

---
+++ src/main/java/com/example/demo/exception/UserNotFoundException.java
@@ -0,0 +1,19 @@
+package com.example.demo.exception;
+
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+@Getter
+@Setter
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException (String message){
+        super(message);
+    }
+}
