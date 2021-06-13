b32029a71d1784f13a7e3b4bfe4fa51104bdd581
Novikov Kirill <kirya.novikov.03@list.ru>
2021-06-13 17:53:41 +0600

    add UserController

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.DTO.UserDto;
+import com.example.demo.exception.UserNotFoundException;
+import com.example.demo.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/getUserById/{userId}")
+    public UserDto getUserById(@PathVariable("userId") Long userId) {
+        var user = userService.getUserById(userId);
+        return UserDto.builder()
+                .name(user.getName())
+                .email(user.getEmail())
+                .build();
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    private String userNotFound(UserNotFoundException uex) {
+        return uex.getMessage();
+    }
+}
