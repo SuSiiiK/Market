c842ef515f29ac3a3e4b7971a4927dae3f40f961
Novikov Kirill <kirya.novikov.03@list.ru>
2021-06-17 18:26:47 +0600

    add ProductRepo

--- src/main/java/com/example/demo/repositories/ProductRepo.java
+++ src/main/java/com/example/demo/repositories/ProductRepo.java
@@ -1,11 +1,16 @@
 package com.example.demo.repositories;
 
 import com.example.demo.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepo extends JpaRepository<Product,Long> {
-
+    Page<Product> findByNameContains(String name , Pageable pageable);
+    Page<Product> findByDescriptionContains (String description, Pageable pageable);
+    Page<Product> findByPriceLessThanEqual (Integer price , Pageable pageable);
+    Page<Product> findByQuantityLessThanEqual (Integer quantity, Pageable pageable);
 }
 
