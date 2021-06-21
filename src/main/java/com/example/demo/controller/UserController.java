953a9747f99f3b2fe79edbe9618b3ec80285b4f3
Novikov Kirill <kirya.novikov.03@list.ru>
2021-06-21 17:28:27 +0600

    update UserController

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,10 +2,17 @@ package com.example.demo.controller;
 
 
 import com.example.demo.DTO.UserDto;
+import com.example.demo.exception.UserRegisterForm;
 import com.example.demo.exception.UserNotFoundException;
 import com.example.demo.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+import java.security.Principal;
 
 @RestController
 @AllArgsConstructor
@@ -17,9 +24,47 @@ public class UserController {
     public UserDto getUserById(@PathVariable("userId") Long userId) {
         var user = userService.getUserById(userId);
         return UserDto.builder()
-                .name(user.getName())
+                .fullname(user.getFullname())
                 .email(user.getEmail())
                 .build();
+    }
+
+    @GetMapping("/profile")
+    public String pageCustomerProfile(Model model, Principal principal)
+    {
+        UserDto user = userService.getByEmail(principal.getName());
+        model.addAttribute("dto", user);
+        return "profile";
+    }
+
+    @GetMapping("/register")
+    public String pageRegisterCustomer(Model model) {
+        if (!model.containsAttribute("dto")) {
+            model.addAttribute("dto", new UserRegisterForm());
+        }
+
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerPage(@Valid UserRegisterForm customerRequestDto,
+                               BindingResult validationResult,
+                               RedirectAttributes attributes) {
+        attributes.addFlashAttribute("dto", customerRequestDto);
+
+        if (validationResult.hasFieldErrors()) {
+            attributes.addFlashAttribute("errors", validationResult.getFieldErrors());
+            return "redirect:/register";
+        }
+
+        userService.register(customerRequestDto);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String loginPage(@RequestParam(required = false, defaultValue = "false") Boolean error, Model model) {
+        model.addAttribute("error", error);
+        return "login";
     }
 
     @ExceptionHandler(UserNotFoundException.class)
