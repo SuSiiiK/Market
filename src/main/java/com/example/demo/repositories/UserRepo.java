96a497328e4786cce1c2d28b6fa3506759c20a63
Novikov Kirill <kirya.novikov.03@list.ru>
2021-06-21 17:30:25 +0600

    update UserService and UserRepo , pom.xml

--- src/main/java/com/example/demo/repositories/UserRepo.java
+++ src/main/java/com/example/demo/repositories/UserRepo.java
@@ -3,5 +3,11 @@ package com.example.demo.repositories;
 import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepo extends JpaRepository<User,Long> {
+
+    boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
