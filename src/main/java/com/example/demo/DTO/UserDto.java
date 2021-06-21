a9b2f89444a843f2f63d49c0c2f9185b7660339d
Novikov Kirill <kirya.novikov.03@list.ru>
2021-06-21 17:28:46 +0600

    update UserDto

--- src/main/java/com/example/demo/DTO/UserDto.java
+++ src/main/java/com/example/demo/DTO/UserDto.java
@@ -1,5 +1,6 @@
 package com.example.demo.DTO;
 
+import com.example.demo.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
@@ -12,9 +13,18 @@ import javax.validation.constraints.NotNull;
 @NoArgsConstructor
 @Builder
 public class UserDto {
+    private int id;
     @NotNull(message = "Please write your name.")
     @NotBlank(message = "Please write name not only with spaces.")
-    private String name;
+    private String fullname;
     @Email(message = "Please write right syntax of your email.")
     private String email;
+
+    public static UserDto from(User user) {
+        return builder()
+                .id(user.getId())
+                .fullname(user.getFullname())
+                .email(user.getEmail())
+                .build();
+    }
 }
