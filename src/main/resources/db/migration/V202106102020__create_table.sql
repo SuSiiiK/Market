0b2973b8dea09d20da57359371df104f256d6ea3
Novikov Kirill <kirya.novikov.03@list.ru>
2021-06-28 14:10:49 +0600

    update db.migration

--- src/main/resources/db/migration/V202106102020__create_table.sql
+++ src/main/resources/db/migration/V202106102020__create_table.sql
@@ -1,59 +1,111 @@
 create table categories
 (
-    id   integer      not null auto_increment,
-    name varchar(255) not null,
-    primary key (id)
-) engine = InnoDB;
+    id   bigint auto_increment
+        primary key,
+    name varchar(255) null
+) ;
 
-create table feedbacks
-(
-    id          integer      not null auto_increment,
-    description varchar(255) not null,
-    stars       integer      not null,
-    product_id  integer,
-    user_id     integer,
-    primary key (id)
-) engine = InnoDB;
-
-create table orders
-(
-    id         integer not null auto_increment,
-    product_id integer not null,
-    user_id    integer not null,
-    primary key (id)
-) engine = InnoDB;
-
-create table products
-(
-    id          integer      not null auto_increment,
-    description varchar(255),
-    image       varchar(255) not null,
-    name        varchar(255) not null,
-    price       integer      not null,
-    quantity    integer      not null,
-    category_id integer      not null,
-    primary key (id)
-) engine = InnoDB;
 
 CREATE TABLE `users` (
-                             `id` int auto_increment NOT NULL,
-                             `email` varchar(128) NOT NULL,
-                             `password` varchar(128) NOT NULL,
-                             `fullname` varchar(128) NOT NULL default ' ',
-                             `enabled` boolean NOT NULL default true,
-                             `role` varchar(16) NOT NULL default 'USER',
-                             PRIMARY KEY (`id`),
-                             UNIQUE INDEX `email_unique` (`email` ASC)
+                         id        bigint not null auto_increment unique
+                             primary key,
+                         email     varchar(255) null,
+                         enabled   bit          null,
+                         password  varchar(255) null,
+                         role      varchar(128) null,
+                         user_name varchar(255) null
 );
 
 
-alter table feedbacks
-    add constraint FKti2ywtwc29ys1i591rmmaveyc foreign key (product_id) references products (id);
-alter table feedbacks
-    add constraint FK312drfl5lquu37mu4trk8jkwx foreign key (user_id) references users (id);
-alter table orders
-    add constraint FKkp5k52qtiygd8jkag4hayd0qg foreign key (product_id) references products (id);
-alter table orders
-    add constraint FK32ql8ubntj5uh44ph9659tiih foreign key (user_id) references users (id);
-alter table products
-    add constraint FKog2rp4qthbtt2lfyhfo32lsw9 foreign key (category_id) references categories (id);
+
+
+create table products
+(
+    id           bigint auto_increment
+        primary key ,
+    description varchar(255),
+    image       varchar(255) not null,
+    name        varchar(255) not null,
+    price       double      not null,
+    quantity    integer      not null,
+    category_id bigint      not null,
+    constraint FKteg3f0l6cndtbn3nsbr1tmxcl
+        foreign key (category_id) references categories (id)
+);
+
+
+create table reviews
+(
+    id      bigint       not null  auto_increment unique primary key ,
+    date    date         null,
+    name    varchar(255) null,
+    product_id bigint       null,
+    user_id bigint       null,
+    constraint FK81nkg9op14w8c6hw7f25f029i
+        foreign key (product_id) references products (id),
+    constraint FKcgy7qjc1r99dp117y9en6lxye
+        foreign key (user_id) references users (id)
+
+);
+
+
+
+create table baskets
+(
+    id      bigint       not null auto_increment
+        primary key,
+    session varchar(255) null,
+    user_id bigint       null,
+    constraint FK87s17cinc4wkx0taas5nh0s8h
+        foreign key (user_id) references users (id)
+
+);
+
+
+create table basket_products
+(
+    id        bigint not null  auto_increment
+        primary key,
+    qty       int    null,
+    basket_id bigint null,
+    product_id   bigint null,
+    constraint FKlomoboid5pugq7ajp2kvqe5id
+        foreign key (basket_id) references baskets (id),
+    constraint FKs86tjrwd3ex57i294fpntxuc6
+        foreign key (product_id) references products (id)
+
+);
+
+create table orders
+(
+    id        bigint auto_increment primary key ,
+    address   varchar(255) null,
+    name      varchar(255) null,
+    tel       varchar(255) null,
+    basket_id bigint       null,
+    total     double       null,
+    constraint FKkeq4ha3u9m7f5nv9wqyk4lnap
+        foreign key (basket_id) references baskets (id)
+);
+
+
+create table tokens
+(
+    id       bigint auto_increment,
+    token    varchar(255) null,
+    users_id bigint       null,
+    constraint tokens_id_uindex
+        unique (id),
+    constraint FKl50lok37qf2734u04knlj57ct
+        foreign key (users_id) references users (id)
+
+);
+
+# alter table basket_products add constraint FKlomoboid5pugq7ajp2kvqe5id foreign key (basket_id) references baskets (id);
+# alter table basket_products add constraint FKco87gmll53y8fpmmhli974xag foreign key (product_id) references products (id);
+# alter table baskets add constraint FK87s17cinc4wkx0taas5nh0s8h foreign key (user_id) references users (id);
+# alter table orders add constraint FKkeq4ha3u9m7f5nv9wqyk4lnap foreign key (basket_id) references baskets (id);
+# alter table products add constraint FKog2rp4qthbtt2lfyhfo32lsw9 foreign key (category_id) references categories (id);
+# alter table reviews add constraint FKpl51cejpw4gy5swfar8br9ngi foreign key (product_id) references products (id);
+# alter table reviews add constraint FKcgy7qjc1r99dp117y9en6lxye foreign key (user_id) references users (id);
+# alter table tokens add constraint FKl50lok37qf2734u04knlj57ct foreign key (users_id) references users (id);
--- src/main/resources/db/migration/V202106171758__data.sql
+++ src/main/resources/db/migration/V202106171758__data.sql
@@ -1,8 +1,12 @@
-insert into categories (name) values ('Note Book') , ('Phone') , ('Watch');
+insert into categories (name) values ('Notebook') , ('Phone') , ('Watch') , ('Power Bank'),('Headphones');
 
 
 insert into products (name, image, quantity, description, price, category_id) values
-('Xiaomi REDMIBOOK 14', 'images/RedMiBook.jpg', 2, 'Note Book', 1000 , 1),
-('Xiaomi Notebook Air 12', 'images/NoteBookAir.jpg', 2, 'NoteBook', 700 , 1),
-('Xiaomi Note 9', 'images/Note8.jpg', 2, 'Phone', 600 , 2),
-('Xiaomi Mi Band 6', 'images/Mi_Band6.jpg', 2, 'Watch', 1000 , 3);
+('Xiaomi REDMIBOOK 14', 'http://www.xiaomi.kg/wp-content/uploads/2019/08/pms_1559024536.53513407.jpg', 1, 'Notebook', 45000 , 1),
+('Xiaomi Notebook Air 12', 'https://object.pscloud.io/cms/cms/Photo/img_0_62_1312_0_1_320.jpg', 1, 'Notebook', 35000 , 1),
+('Xiaomi Note 9', 'https://mobile-review.com/news/wp-content/uploads/ESJIIZ_W4AEdSx2-1.jpg', 1, 'Phone', 23000 , 2),
+('Xiaomi Mi Band 6', 'https://mi-shop.kg/wp-content/uploads/2021/04/ab2665d8_c73b_11ea_8109_2cfda1e15e3a_43b59bb2_913a_11eb_f797_fa163e0bb06b.resize1-600x600.jpg', 1, 'Watch', 2500 , 3),
+('Xiaomi Mi Power Bank','https://mi-shop.kg/wp-content/uploads/2021/05/332232323232323.jpg',1,'Power Bank Pikachu Edition 10000 mAh', 1900, 4),
+('Xiaomi Redmi Airdots 3','http://www.xiaomi.kg/wp-content/uploads/2021/06/7i.jpg',1,'Беспроводные наушники Xiaomi Redmi Airdots 3 True Wireless Bluetooth Headset',3990,5),
+('Xiaomi Mi Noise Cancelling Earphones 3.5 Jack','http://www.xiaomi.kg/wp-content/uploads/2020/08/nayshniki_xiaomi_mi_noise_cancelling_earphones_jzej02jy_1.jpg',1,'Стерео-наушники Xiaomi Mi Noise Cancelling Earphones 3.5 Jack',1300,5),
+('Xiaomi Mi Note 10 Lite EU', 'http://www.xiaomi.kg/wp-content/uploads/2020/05/F4L-white-800-1.png',1,'Xiaomi MI 10 Lite EU',45000,2);
