526546146e3bea12ab9410393105ba60ccb9fa75
Novikov Kirill <kirya.novikov.03@list.ru>
2021-06-28 14:03:11 +0600

    .

--- src/main/java/com/example/demo/repositories/FeedbackRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repositories;
-
-import com.example.demo.entity.Feedback;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FeedbackRepo extends JpaRepository<Feedback,Long> {
-}
